4429e3ada6a6394a7f564ef9e5f685a485b899c2
Dev <flashytesting@gmail.com>
2023-08-26 21:55:53 +0530

    Update theme button element selection

--- script.js
+++ script.js
@@ -15,24 +15,25 @@ navLinks.forEach(navLink => {
 })
 
 // Theme toggle stuff
-const themeButton = document.getElementById('theme-button');
+const themeButtons = document.querySelectorAll('#theme-button');
 const links = document.querySelectorAll('a');
 const burgerBars = document.querySelectorAll('#nav-button > *');
 
-themeButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-    
-    links.forEach(link => link.classList.toggle('dark-theme-link'));
-    burgerBars.forEach(bar => bar.classList.toggle('dark-theme-burger-bar'));
+themeButtons.forEach(themeButton => {
+    themeButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
+        
+        links.forEach(link => link.classList.toggle('dark-theme-link'));
+        burgerBars.forEach(bar => bar.classList.toggle('dark-theme-burger-bar'));
 
-    if (document.body.classList.contains('dark-theme')) {
-        themeButton.innerHTML = '&#9789;';
-        themeButton.style.fontSize = '1.5rem'
-    } else {
-        themeButton.innerHTML = '&#9788;';
-        themeButton.style.fontSize = '2rem'
-    }
+        if (document.body.classList.contains('dark-theme')) {
+            themeButton.innerHTML = '&#9789;';
+            themeButton.style.fontSize = '1.5rem'
+        } else {
+            themeButton.innerHTML = '&#9788;';
+            themeButton.style.fontSize = '2rem'
+        }
+    })
 })
-
 
 
